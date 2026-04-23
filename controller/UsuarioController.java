ce98656d9d0c80a5738c3d5305b2c6882ac49daf
iago souza <iagosouzza539@gmail.com>
2026-04-23 19:32:05 -0300

    feat: Criação dos endpoints, cadastrar e atualizar.

--- controller/UsuarioController.java
+++ controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.senac.fiscalizei.controller;
 
 import com.senac.fiscalizei.dto.LoginDTO;
+import com.senac.fiscalizei.dto.UsuarioDTO;
 import com.senac.fiscalizei.model.Usuario;
 import com.senac.fiscalizei.service.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,21 +18,29 @@ import java.util.List;
 public class UsuarioController {
     private final UsuarioService service;
 
-    public UsuarioController(UsuarioService service){
+    public UsuarioController(UsuarioService service) {
         this.service = service;
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Usuario>> todosUsuarios(){
+    public ResponseEntity<List<Usuario>> todosUsuarios() {
         return ResponseEntity.ok(service.listarTodos());
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Usuario> login(@RequestBody @Valid LoginDTO loginDTO){
+    public ResponseEntity<Usuario> login(@RequestBody @Valid LoginDTO loginDTO) {
         Usuario usuario = service.login(loginDTO);
 
         return ResponseEntity.ok(usuario);
     }
 
+    @PostMapping("/")
+    public ResponseEntity<Usuario> cadastrar(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criar(usuarioDTO));
+    }
 
+    @PutMapping("/")
+    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @RequestBody @Valid UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(service.atualizar(id, usuarioDTO));
+    }
 }
