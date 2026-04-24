ffb47e961a0b6f6d13fa3dec7050c94274ad5382
Iago Souza <124836944+IagoSouzas@users.noreply.github.com>
2026-04-24 17:16:26 -0300

    Add files via upload

---
+++ fiscalizei/src/main/java/com/senac/fiscalizei/FiscalizeiApplication.java
@@ -0,0 +1,13 @@
+package com.senac.fiscalizei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalizeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalizeiApplication.class, args);
+	}
+
+}
